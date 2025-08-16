4b1555b5a64c73ac138166dc263f9316b0b8a623
Leandro S Ferreira <contato@analiticasolucoes.com.br>
2025-08-16 08:53:51 -0300

    Mudanca na estrutura de diretorios em preparacao para sistema de url/uri amigavel

--- views/home.php
+++ views/home.php
@@ -13,22 +13,22 @@
         <title>Analítica Soluções</title>
         <meta name="description" content="Desenvolvimento de Sistemas e Webdesign | Analítica Soluções - Soluções Digitais Inovadoras">
         <link rel="canonical" href="https://analiticasolucoes.com.br" />
-        <link rel="icon" type="image/x-icon" href="img/ico/favicon.ico">
-        <link rel="preload" as="image" href="img/achar-marca.webp">
+        <link rel="icon" type="image/x-icon" href="assets/img/ico/favicon.ico">
+        <link rel="preload" as="image" href="assets/img/achar-marca.webp">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-        <link href="css/estilo.css" rel="stylesheet">
+        <link href="assets/css/estilo.css" rel="stylesheet">
     </head>
     <body>
         <header class="cabecalho fixed-top">
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="https://analiticasolucoes.com.br">
-                        <img src="img/analitica-sistemas-logo.webp" alt="logomarca oficial da analitica sistemas">
+                        <img src="assets/img/analitica-sistemas-logo.webp" alt="logomarca oficial da analitica sistemas">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -50,7 +50,7 @@
                             <li class="nav-item">
                                 <a href="#contato" class="nav-link">Contato</a>
                             </li>
-                        </ul> 
+                        </ul>
                     </div>
                 </div>
             </nav>
@@ -71,35 +71,35 @@
                 <div class="card-container">
                     <div class="card">
                         <h3 class="solucao-title">Páginas<br>Dinâmicas</h3>
-                        <img class="solucao-img" src="img/solucoes/paginas-dinamicas.webp" alt="icone representando páginas dinamicas" loading="lazy">
+                        <img class="solucao-img" src="assets/img/solucoes/paginas-dinamicas.webp" alt="icone representando páginas dinamicas" loading="lazy">
                         <p class="solucao-parag">
                             Criação de <strong>páginas dinâmicas</strong> com foco em micro, pequenas empresas e prestadores de serviços.
                         </p>
                     </div>
                     <div class="card">
                         <h3 class="solucao-title">Gestão de<br>conteúdo</h3>
-                        <img class="solucao-img" src="img/solucoes/gestao-conteudo.webp" alt="icone representando gestão de conteúdo" loading="lazy">
+                        <img class="solucao-img" src="assets/img/solucoes/gestao-conteudo.webp" alt="icone representando gestão de conteúdo" loading="lazy">
                         <p class="solucao-parag">
                             Garantimos que o <strong>conteúdo</strong> do seu site esteja <strong>atualizado e relevante</strong>.
                         </p>
                     </div>
                     <div class="card">
                         <h3 class="solucao-title">SEO - Search<br>Engine Optimization</h3>
-                        <img class="solucao-img" src="img/solucoes/seo-otimizacao.webp" alt="icone representando otimizacao de SEO" loading="lazy">
+                        <img class="solucao-img" src="assets/img/solucoes/seo-otimizacao.webp" alt="icone representando otimizacao de SEO" loading="lazy">
                         <p class="solucao-parag">
                             Criação e acompanhamento de <strong>estratégias para otimização de resultados</strong> nos mecanismos de buscas.
                         </p>
                     </div>
                     <div class="card">
                         <h3 class="solucao-title">Captação de<br>Leads</h3>
-                        <img class="solucao-img" src="img/solucoes/captura-leads.webp" alt="icone representando captacao de leads" loading="lazy">
+                        <img class="solucao-img" src="assets/img/solucoes/captura-leads.webp" alt="icone representando captacao de leads" loading="lazy">
                         <p class="solucao-parag">
                             <strong>Conquista de novos clientes</strong> para o seu negócio por meio de técnicas de gestão de tráfego (Tráfego Orgânico e Tráfego Pago).
                         </p>
                     </div>
                     <div class="card">
                         <h3 class="solucao-title">Redes<br>Sociais</h3>
-                        <img class="solucao-img" src="img/solucoes/integracao-redes-sociais.webp" alt="icone representando integracao com redes sociais" loading="lazy">
+                        <img class="solucao-img" src="assets/img/solucoes/integracao-redes-sociais.webp" alt="icone representando integracao com redes sociais" loading="lazy">
                         <p class="solucao-parag">
                             <strong>Integração entre sua página e seus perfis nas redes sociais</strong> (Instagram, Facebook, Google Empresas, etc.) com geração de conteúdo dinâmico.
                         </p>
@@ -111,7 +111,7 @@
             <article>
                 <h1><a name="sobre"></a>Sobre</h1>
                 <p>Por que <strong>escolher a Analítica Soluções</strong>?</p>
-                <p>Somos uma <strong>empresa capixaba</strong> com foco na <strong>prestação de serviços com alta qualidade</strong> e <strong>soluções personalizadas</strong> 
+                <p>Somos uma <strong>empresa capixaba</strong> com foco na <strong>prestação de serviços com alta qualidade</strong> e <strong>soluções personalizadas</strong>
                     para melhor atender às necessidades dos nossos clientes. A Analítica Soluções <strong>usa as melhores técnicas disponíveis</strong> no desenvolvimento de páginas dinâmicas para o seu negócio.
                 <p>Enquanto o mercado oferece plataformas prontas e sites genéricos desenvolvidos com frameworks engessados, aqui <strong>na Analítica Soluções priorizamos o desenvolvimento
                         de páginas únicas, originais e modernas</strong> que permitirão ao seu negócio ter uma identidade diferenciada na Internet e perante seus clientes.</p>
@@ -165,35 +165,35 @@
                     </div>
                 </div>
             </article>
-        </section> 
+        </section>
         <section class="portifolio d-flex">
             <article class="align-itens-center justify-content-center">
                 <h1><a name="portifolio"></a>Portifólio</h1>
                 <p class="text-center">Conheça alguns de nossos <strong>projetos</strong>:</p>
-                <div class="galeria row flex-row align-itens-center justify-content-center">    
+                <div class="galeria row flex-row align-itens-center justify-content-center">
                     <div class="img-galeria col-12 col-lg-3 text-center my-4 w-auto">
                         <a href="https://dilameti.com.br" target="_blank">
-                            <img src="img/portifolio/case-01.webp" class="img-thumbnail text-center" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Dilameti Express">
+                            <img src="assets/img/portifolio/case-01.webp" class="img-thumbnail text-center" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Dilameti Express">
                         </a>
                     </div>
                     <div class="img-galeria col-12 col-lg-3 text-center my-4 w-auto">
                         <a href="https:/oresgatedofeminino.com.br/" target="_blank">
-                            <img src="img/portifolio/case-02.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para Flavia Moura">
+                            <img src="assets/img/portifolio/case-02.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para Flavia Moura">
                         </a>
                     </div>
                     <div class="img-galeria col-12 col-lg-3 text-center my-4 w-auto">
                         <a href="https://jreisclimatizacao.com.br/" target="_blank">
-                            <img src="img/portifolio/case-03.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa J Reis Climatização">
+                            <img src="assets/img/portifolio/case-03.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa J Reis Climatização">
                         </a>
                     </div>
                     <div class="img-galeria col-12 col-lg-3 text-center my-4 w-auto">
                         <a href="https://sistema.mathiasebernardes.com.br/" target="_blank">
-                            <img src="img/portifolio/case-04.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Mathias e Bernades Advogados">
+                            <img src="assets/img/portifolio/case-04.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Mathias e Bernades Advogados">
                         </a>
                     </div>
                     <div class="img-galeria col-12 col-lg-3 text-center my-4 w-auto">
                         <a href="https://hypercomputerbarra.com.br/" target="_blank">
-                            <img src="img/portifolio/case-05.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Hyper Computer">
+                            <img src="assets/img/portifolio/case-05.webp" class="img-thumbnail" loading="lazy" alt="Imagem em miniatura do site desenvolvido para a empresa Hyper Computer">
                         </a>
                     </div>
                 </div>
@@ -203,16 +203,16 @@
             <article>
                 <h1><a name="contato"></a>Contato</h1>
                 <p>Entre em contato e <strong>peça um orçamento</strong> personalizado:</p>
-                <form class="formulario" action="mail/contato.php" method="post">
+                <form class="formulario" action="/contato" method="post">
                     <input name="nome" type="text" class="form-control formulario-contato" id="nome" placeholder="Nome" required>
                     <input name="email" type="email" class="form-control formulario-contato" id="email" placeholder="E-mail" required>
                     <input name="assunto" type="text" class="form-control formulario-contato" id="assunto" placeholder="Assunto">
                     <textarea name="mensagem" rows="7" class="form-control formulario-contato" id="mensagem" placeholder="Mensagem"></textarea>
                     <input name="submit" type="submit" class="btn btn-primary btn-lg text-center mx-auto d-block formulario-contato" id="submit" value="Enviar">
-                    <span style="display:none;visibility:hidden;">  
+                    <span style="display:none;visibility:hidden;">
                         <label for="emails">
                         Este campo é apenas para prevenção de spammers. Por favor, não altere seu conteudo ou sua mensagem não será enviada!
-                        </label>  
+                        </label>
                         <input type="text" name="emails" size="1" value="" />
                     </span>
                 </form>
@@ -220,17 +220,17 @@
         </section>
         <div class="whatsapp-button">
             <a class="whatsapp-link" href="https://wa.me/5527988826711?text=Acabo de visitar seu site e gostaria de mais informações por favor." target="_blank">
-                <img class="whatsapp-img" src="img/whatsapp.webp" alt="..." loading="lazy">
+                <img class="whatsapp-img" src="assets/img/whatsapp.webp" alt="..." loading="lazy">
             </a>
         </div>
         <footer class="rodape">
             <span class="copyright">Copyright © 2023 | <strong>Analítica Soluções.</strong></span>
             <div class="social-media-icons">
-                Siga a gente nas redes sociais: 
+                Siga a gente nas redes sociais:
                 <a class="social-media-icon-link py-1" href="https://www.facebook.com/profile.php?id=61550039357070&__cft__[0]=AZXKejzvhPl_YaiPudcWciZFJLoeMfi2zanjJazjote7mdn9JksJndWe1Jl85ODUDaX58vPOgfpTdNSvekl-P6ap_IB1chwz4XnECrcRaZelWRUBMn6HBDObY1DiHeaTbhaqzFX-b8AfiLg6TYo7mD1NDaL4ff0aYXI_ruNIxY2W4KRTE4eIwOGD2UmBCOvvNF0&__tn__=-UC%2CP-R" target="_blank">
                     <i class="bi bi-facebook"></i>
                     Facebook
-                </a> 
+                </a>
                 <a class="social-media-icon-link py-1" href="https://www.instagram.com/analiticasolucoes.es/" target="_blank">
                     <i class="bi bi-instagram"></i>
                     Instagram
@@ -238,7 +238,7 @@
                 <a class="social-media-icon-link py-1" href="https://goo.gl/maps/E3VvBtVBFYHGXzhW9" target="_blank">
                     <i class="bi bi-google"></i>
                     Google Meu Negócio
-                </a> 
+                </a>
             </div>
         </footer>
     </body>
